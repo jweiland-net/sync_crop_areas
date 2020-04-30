75cb2062aeeb99adba5b0f17569776ff13333168
Stefan Froemken <froemken@gmail.com>
2020-04-30 15:48:23 +0200

    Change copy_crop_are column to sync_crop_are

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,5 +2,5 @@
 # Table structure for table 'sys_file_reference'
 #
 CREATE TABLE sys_file_reference (
-	copy_crop_area tinyint(1) unsigned DEFAULT '1' NOT NULL,
+	sync_crop_area tinyint(1) unsigned DEFAULT '1' NOT NULL,
 );
