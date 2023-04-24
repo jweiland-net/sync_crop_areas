91ec48530e34e3d7a8755ff867ab0f1f4e5e9d79
Stefan Froemken <sfroemken@jweiland.net>
2023-04-24 16:35:23 +0200

    Update version to 2.2.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Sync Crop Areas',
     'description' => 'Sync first found crop area to all other CropVariants',
-    'version' => '2.2.1',
+    'version' => '2.2.2',
     'category' => 'plugin',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.4.0-8.99.99',
-            'typo3' => '10.4.19-11.5.99',
+            'typo3' => '10.4.36-11.5.99',
         ],
         'conflicts' => [
         ],
