d745fbcae8e6f45d954bb6493284231edab4d90b
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-11-27 12:20:48 +0100

    [TASK] Fixed new ersion related infos

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,23 +1,28 @@
 <?php
 
+/*
+ * This file is part of the package jweiland/sync_crop_areas.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Sync Crop Areas',
     'description' => 'Sync first found crop area to all other CropVariants',
-    'version' => '3.0.0',
+    'version' => '4.0.0',
     'category' => 'plugin',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'author' => 'Stefan Froemken',
+    'author' => 'Stefan Froemken, Hoja Mustaffa Abdul Latheef',
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.99.99',
-            'typo3' => '11.5.23-12.4.99',
+            'php' => '8.2-8.99.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
+        'conflicts' => [],
+        'suggests' => [],
     ],
 ];
